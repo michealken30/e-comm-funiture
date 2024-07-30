d6037355fa96df78e22be1ad7d62b2c32fc7f2ca
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-07-31 00:04:56 +0100

    sign in backend functionality

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const Db = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://kennethoshogwe:BVIupTIqnC02B5DZ@cluster0.nl8vlqr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    )
-    .then(() => {
-      console.log("database connected ");
-    });
+  await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("database connected ");
+  });
 };
