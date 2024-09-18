e60d2d5d9eb171a40e944ca17a7c66a48ea593eb
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-09-18 15:17:15 +0100

    fixed some bugs reletated

--- backend/contollers/orderController.js
+++ backend/contollers/orderController.js
@@ -5,9 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
-
-  console.log(req.body);
+  const frontend_url = "https://alpha-furniture-frontend.onrender.com";
 
   try {
     const newOrder = new orderModel({
